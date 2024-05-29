f1a8845047bd28acee30628cd9b0ed233d11ec3b
Diego Veras <diego.veras@gmail.com>
2024-05-29 20:08:04 -0300

    Inseri tabela e pontuacao

--- campeonatos/models.py
+++ campeonatos/models.py
@@ -1,7 +1,8 @@
 # campeonatos/models.py
 
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Campeonato(models.Model):
     TIPO_CHOICES = [
@@ -17,6 +18,41 @@ class Campeonato(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def get_jogos_validos(self):
+        return self.jogos.filter(gols_time_casa__isnull=False, gols_time_fora__isnull=False, data__range=(self.data_inicio, self.data_fim))
+
+    def get_classificacao(self):
+        times = self.times.all()
+        classificacao = {time: {'pontos': 0, 'gols_pro': 0, 'gols_contra': 0, 'saldo': 0, 'jogos': 0} for time in times}
+        
+        for jogo in self.get_jogos_validos():
+            time_casa = jogo.time_casa
+            time_fora = jogo.time_fora
+            gols_casa = jogo.gols_time_casa
+            gols_fora = jogo.gols_time_fora
+
+            classificacao[time_casa]['gols_pro'] += gols_casa
+            classificacao[time_casa]['gols_contra'] += gols_fora
+            classificacao[time_fora]['gols_pro'] += gols_fora
+            classificacao[time_fora]['gols_contra'] += gols_casa
+
+            classificacao[time_casa]['jogos'] += 1
+            classificacao[time_fora]['jogos'] += 1
+
+            if gols_casa > gols_fora:
+                classificacao[time_casa]['pontos'] += 3
+            elif gols_fora > gols_casa:
+                classificacao[time_fora]['pontos'] += 3
+            else:
+                classificacao[time_casa]['pontos'] += 1
+                classificacao[time_fora]['pontos'] += 1
+        
+        for time in classificacao:
+            classificacao[time]['saldo'] = classificacao[time]['gols_pro'] - classificacao[time]['gols_contra']
+
+        classificacao = sorted(classificacao.items(), key=lambda x: (x[1]['pontos'], x[1]['gols_pro'], x[1]['saldo']), reverse=True)
+        return classificacao
 
 class Time(models.Model):
     nome = models.CharField(max_length=100)
